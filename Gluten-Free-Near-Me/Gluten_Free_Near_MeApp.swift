9ce9ba6a674b6604c5a5428b9b4160881c16741b
Daniel Sooknanan <daniel.sooknanan216@gmail.com>
2024-10-25 20:50:02 -0400

    Initial Commit

---
+++ Gluten-Free-Near-Me/Gluten_Free_Near_MeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gluten_Free_Near_MeApp.swift
+//  Gluten-Free-Near-Me
+//
+//  Created by Daniel S. on 10/25/24.
+//
+
+import SwiftUI
+
+@main
+struct Gluten_Free_Near_MeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
