4d7355051f89619443847b036dd99080f0cb2c4e
Daniel Sooknanan <daniel.sooknanan216@gmail.com>
2024-10-30 22:43:10 -0400

    Add button styling. Add panning on button press.

---
+++ Gluten-Free-Near-Me/Restaurants/ViewButton.swift
@@ -0,0 +1,18 @@
+//
+//  ViewButton.swift
+//  Gluten-Free-Near-Me
+//
+//  Created by Daniel S. on 10/30/24.
+//
+
+import SwiftUI
+
+struct ViewButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ViewButton()
+}
