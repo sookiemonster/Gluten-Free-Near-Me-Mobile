7839a723bf1faea4daf997153204cbac9cf47a22
Daniel Sooknanan <daniel.sooknanan216@gmail.com>
2024-11-01 12:58:29 -0400

    Refactor for modularity; move RestaurantMarker into its own View.

---
+++ Gluten-Free-Near-Me/Map/RestaurantMarker.swift
@@ -0,0 +1,18 @@
+//
+//  RestaurantMarker.swift
+//  Gluten-Free-Near-Me
+//
+//  Created by Daniel S. on 11/1/24.
+//
+
+import SwiftUI
+
+struct RestaurantMarker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RestaurantMarker()
+}
